acbb4451e1b5598f59d017398b7d89f2377d1813
Jack <jackubc2013@gmail.com>
2021-02-03 23:52:05 -0800

    Implement extension

---
+++ js/chessMoveReminder.js
@@ -0,0 +1,93 @@
+let observer;
+let recentRandomNumber = 0;
+
+function randomPositiveNumber(max) {
+    return Math.floor(Math.random() * max) + 1;
+}
+
+function semiRandomPositiveNumber(max) {
+    let result = randomPositiveNumber(max);
+    while (recentRandomNumber === result) {
+        result = randomPositiveNumber(max);
+    }
+    recentRandomNumber = result;
+    return result;
+}
+
+function parseSecondsFromClock(clock) {
+    const regex = /((\d):)?((\d)?\d):(\d\d)/;
+    let match = clock.match(regex);
+    let hour = parseInt(match[2] ? match[2] : 0);
+    let min = parseInt(match[3]);
+    let sec = parseInt(match[5]);
+    return sec + min * 60 + hour * 3600;
+}
+
+function chessMoveReminder() {
+    let timer;
+    let seconds = 5;
+    let audio;
+
+    let playAudio = function(who) {
+        let audioUrl = 'audio/SoundEffect/SoundEffect.mp3';
+        if (who === 'HikaruGotham') {
+            audioUrl = 'audio/HikaruGotham/' + semiRandomPositiveNumber(20) + '.wav';
+        }
+        audio = new Audio(chrome.runtime.getURL(audioUrl));
+        try {
+            audio.play();
+        } catch(err) {
+            console.log('Unable to play audio: ' + err.message);
+        }
+    }
+
+    let target = document.querySelector('#board-layout-player-bottom .clock-component');
+
+    if (target !== null) {
+        if (observer !== undefined) {
+            observer.disconnect();
+        }
+        observer = new MutationObserver(function(mutations) {
+            mutations.forEach(function(mutation) {
+                let currentClock = mutation.target.attributes.getNamedItem('data-clock').value;
+                clearTimeout(timer);
+                let isTurn = mutation.target.classList.contains('clock-playerTurn');
+                if (!isTurn && audio !== undefined) {
+                    audio.pause();
+                }
+                if (isTurn) {
+                    chrome.storage.sync.get({
+                        'who': 'HikaruGotham',
+                        'number': 60,
+                        'type': 'seconds'
+                    }, function(data) {
+                        let who = data.who;
+                        let number = data.number;
+                        let type = data.type;
+
+                        let timeToWait;
+
+                        if (type === 'percentage') {
+                            let seconds = parseSecondsFromClock(currentClock) * number * 10;
+                            timeToWait = Math.max(seconds, 2000);
+                        } else {
+                            timeToWait = number * 1000;
+                        }
+                        timer = setTimeout(playAudio, timeToWait, who);
+                    });
+                }
+            });
+        });
+
+        observer.observe(target, {
+            attributes:true,
+            attributeFilter:['class']
+        });
+    }
+}
+
+setTimeout(chessMoveReminder, 1000);
+
+window.addEventListener('hashchange', function() {
+    setTimeout(chessMoveReminder, 1000);
+});
---
+++ js/popup.js
@@ -0,0 +1,55 @@
+function updateElement(elementId, value) {
+    let element = document.getElementById(elementId);
+    element.value = value;
+}
+
+function update() {
+    chrome.storage.sync.get({
+        'who': 'HikaruGotham',
+        'number': 60,
+        'type': 'seconds'
+    }, function(data) {
+        updateElement('who', data.who);
+        updateElement('number', data.number);
+        updateElement('type', data.type);
+    });
+}
+
+let alertTimer;
+
+function showAlert(id) {
+    clearTimeout(alertTimer);
+    $('#' + id).animate({ opacity: 100 }, 0).show();
+    alertTimer = setTimeout(function() {
+        $('#' + id).animate({ opacity: 0 }, 500).hide('slow');
+    }, 3000);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    update();
+
+    let saveButton = document.getElementById('save');
+    saveButton.addEventListener('click', function() {
+        let who = document.getElementById('who').value;
+        let number = parseFloat(document.getElementById('number').value);
+        let type = document.getElementById('type').value;
+
+        if (who !== 'HikaruGotham' && who !== 'SoundEffect') {
+            showAlert('invalidError');
+        } else if (type !== 'seconds' && type !== 'percentage') {
+            showAlert('invalidError');
+        } else if (number < 0) {
+            showAlert('negativeError');
+        } else if (type === 'percentage' && number > 100) {
+            showAlert('percentageError');
+        } else {
+            chrome.storage.sync.set({
+                who: who,
+                number: number,
+                type: type
+            });
+            showAlert('savedSuccessfully');
+        }
+    }, false);
+}, false);
