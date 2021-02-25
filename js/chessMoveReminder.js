ca0cbc76a2bab4117de9b3e1576e62754c66ffee
Hansel Lee <mrhansellee@gmail.com>
2021-02-25 13:28:09 -0800

    Fixed indentation

--- js/chessMoveReminder.js
+++ js/chessMoveReminder.js
@@ -2,103 +2,109 @@ let observer;
 let recentRandomNumber = 0;
 
 function randomPositiveNumber(max) {
-  return Math.floor(Math.random() * max) + 1;
+    return Math.floor(Math.random() * max) + 1;
 }
 
 function randomPositiveNumberWithoutRepeat(max) {
-  let result;
-  do {
-    result = randomPositiveNumber(max);
-  } while (recentRandomNumber === result);
+    let result;
+    do {
+        result = randomPositiveNumber(max);
+    } while (recentRandomNumber === result);
 
-  recentRandomNumber = result;
-  return result;
+    recentRandomNumber = result;
+    return result;
 }
 
 function parseSecondsFromClock(clock) {
-  const regex = /((\d):)?((\d)?\d):(\d\d)/;
-  let match = clock.match(regex);
-  let hour = parseInt(match[2] ? match[2] : 0);
-  let min = parseInt(match[3]);
-  let sec = parseInt(match[5]);
-  return sec + min * 60 + hour * 3600;
+    const regex = /((\d):)?((\d)?\d):(\d\d)/;
+    let match = clock.match(regex);
+    let hour = parseInt(match[2] ? match[2] : 0);
+    let min = parseInt(match[3]);
+    let sec = parseInt(match[5]);
+    return sec + min * 60 + hour * 3600;
 }
 
 function chessMoveReminder() {
-  let timer;
-  let audio;
+    let timer;
+    let audio;
 
-  let playAudio = async function (who) {
-    let audioUrl = "audio/SoundEffect/SoundEffect.mp3";
-    if (who === "HikaruGotham") {
-      audioUrl =
-        "audio/HikaruGotham/" + randomPositiveNumberWithoutRepeat(29) + ".wav";
-    }
-    audio = new Audio(chrome.runtime.getURL(audioUrl));
-    try {
-      await audio.play();
-      console.log("play");
-    } catch (err) {
-      console.log("MOVE Extension: Unable to play audio: " + err.message);
-    }
-  };
-  let target =
-    document.querySelector("#board-layout-player-bottom .clock-component") ||
-    document.querySelector(".rclock-bottom");
-  if (target !== null) {
-    if (observer !== undefined) {
-      observer.disconnect();
-    }
-    observer = new MutationObserver(function (mutations) {
-      let mutation = mutations[0];
-      let currentClock =
-        mutation.target.querySelector(".time") || mutation.target;
-      clearTimeout(timer);
-      // For some reason chess.com/live#g=xxx uses clock-playerTurn
-      // whereas chess.com/game/live/xxx uses clock-player-turn
-      let isTurn =
-        mutation.target.classList.contains("clock-playerTurn") ||
-        mutation.target.classList.contains("clock-player-turn") ||
-        mutation.target.classList.contains("running");
-      if (!isTurn && audio !== undefined) {
-        audio.pause();
-      }
-      if (isTurn) {
-        chrome.storage.sync.get(
-          {
-            who: "HikaruGotham",
-            number: 60,
-            type: "seconds",
-          },
-          function (data) {
-            let who = data.who;
-            let number = data.number;
-            let type = data.type;
-
-            let timeToWait;
-
-            if (type === "percentage") {
-              let seconds = parseSecondsFromClock(currentClock) * number * 10;
-              timeToWait = Math.max(seconds, 2000);
-            } else {
-              timeToWait = number * 1000;
+    let playAudio = async function (who) {
+        let audioUrl = "audio/SoundEffect/SoundEffect.mp3";
+        if (who === "HikaruGotham") {
+            audioUrl =
+                "audio/HikaruGotham/" +
+                randomPositiveNumberWithoutRepeat(29) +
+                ".wav";
+        }
+        audio = new Audio(chrome.runtime.getURL(audioUrl));
+        try {
+            await audio.play();
+            console.log("play");
+        } catch (err) {
+            console.log("MOVE Extension: Unable to play audio: " + err.message);
+        }
+    };
+    let target =
+        document.querySelector(
+            "#board-layout-player-bottom .clock-component"
+        ) || document.querySelector(".rclock-bottom");
+    if (target !== null) {
+        if (observer !== undefined) {
+            observer.disconnect();
+        }
+        observer = new MutationObserver(function (mutations) {
+            let mutation = mutations[0];
+            let currentClock =
+                mutation.target.querySelector(".time") || mutation.target;
+            clearTimeout(timer);
+            // For some reason chess.com/live#g=xxx uses clock-playerTurn
+            // whereas chess.com/game/live/xxx uses clock-player-turn
+            let isTurn =
+                mutation.target.classList.contains("clock-playerTurn") ||
+                mutation.target.classList.contains("clock-player-turn") ||
+                mutation.target.classList.contains("running");
+            if (!isTurn && audio !== undefined) {
+                audio.pause();
             }
-            timer = setTimeout(playAudio, timeToWait, who);
-          }
-        );
-      }
-    });
+            if (isTurn) {
+                chrome.storage.sync.get(
+                    {
+                        who: "HikaruGotham",
+                        number: 60,
+                        type: "seconds",
+                    },
+                    function (data) {
+                        let who = data.who;
+                        let number = data.number;
+                        let type = data.type;
 
-    observer.observe(target, {
-      attributes: true,
-      attributeFilter: ["class"],
-      attributeOldValue: true,
-    });
-  }
+                        let timeToWait;
+
+                        if (type === "percentage") {
+                            let seconds =
+                                parseSecondsFromClock(currentClock) *
+                                number *
+                                10;
+                            timeToWait = Math.max(seconds, 2000);
+                        } else {
+                            timeToWait = number * 1000;
+                        }
+                        timer = setTimeout(playAudio, timeToWait, who);
+                    }
+                );
+            }
+        });
+
+        observer.observe(target, {
+            attributes: true,
+            attributeFilter: ["class"],
+            attributeOldValue: true,
+        });
+    }
 }
 
 setTimeout(chessMoveReminder, 1000);
 
 window.addEventListener("hashchange", function () {
-  setTimeout(chessMoveReminder, 1000);
+    setTimeout(chessMoveReminder, 1000);
 });
