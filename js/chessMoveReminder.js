0a4eff8e30a589753fdfbf7e9fa7ff8d42d8f536
Hansel Lee <mrhansellee@gmail.com>
2021-02-27 19:58:00 -0800

    refactor: object destructuring, reordered if statements

--- js/chessMoveReminder.js
+++ js/chessMoveReminder.js
@@ -43,60 +43,57 @@ function chessMoveReminder() {
             console.log('MOVE Extension: Unable to play audio: ' + err.message);
         }
     };
+
     let target =
         document.querySelector(
             '#board-layout-player-bottom .clock-component'
         ) || document.querySelector('.rclock-bottom');
+
     if (target !== null) {
         if (observer !== undefined) {
             observer.disconnect();
         }
+
         observer = new MutationObserver(function (mutations) {
             let mutation = mutations[0];
             let currentClock =
                 mutation.target.querySelector('.time') || mutation.target;
             currentClock = currentClock.innerText.replace('/\n/g', '');
             clearTimeout(timer);
+
             // For some reason chess.com/live#g=xxx uses clock-playerTurn
             // whereas chess.com/game/live/xxx uses clock-player-turn
             let isTurn =
                 mutation.target.classList.contains('clock-playerTurn') ||
                 mutation.target.classList.contains('clock-player-turn') ||
                 mutation.target.classList.contains('running');
-            if (
-                audio !== undefined &&
-                mutation.oldValue !== mutation.target.classList.value
-            )
-                audio.pause();
-            if (
-                isTurn &&
-                mutation.oldValue !== mutation.target.classList.value
-            ) {
-                chrome.storage.sync.get(
-                    {
-                        who: 'HikaruGotham',
-                        number: 60,
-                        type: 'seconds',
-                    },
-                    function (data) {
-                        // let who = data.who;
-                        // let number = data.number;
-                        // let type = data.type;
-                        let { who, number, type } = data;
-                        let timeToWait;
 
-                        if (type === 'percentage') {
-                            let seconds =
-                                parseSecondsFromClock(currentClock) *
-                                number *
-                                10;
-                            timeToWait = Math.max(seconds, 2000);
-                        } else {
-                            timeToWait = number * 1000;
+            if (mutation.oldValue !== mutation.target.classList.value) {
+                if (audio !== undefined) audio.pause();
+                if (isTurn) {
+                    chrome.storage.sync.get(
+                        {
+                            who: 'HikaruGotham',
+                            number: 60,
+                            type: 'seconds',
+                        },
+                        function (data) {
+                            let { who, number, type } = data;
+                            let timeToWait;
+
+                            if (type === 'percentage') {
+                                let seconds =
+                                    parseSecondsFromClock(currentClock) *
+                                    number *
+                                    10;
+                                timeToWait = Math.max(seconds, 2000);
+                            } else {
+                                timeToWait = number * 1000;
+                            }
+                            timer = setTimeout(playAudio, timeToWait, who);
                         }
-                        timer = setTimeout(playAudio, timeToWait, who);
-                    }
-                );
+                    );
+                }
             }
         });
 
