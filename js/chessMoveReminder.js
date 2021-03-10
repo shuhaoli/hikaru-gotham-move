88d46d27f0c8f962e9a0800f76c46e45c5da999e
Jack <jackubc2013@gmail.com>
2021-03-09 20:24:38 -0800

    Fix sound playing at beginning of next game

--- js/chessMoveReminder.js
+++ js/chessMoveReminder.js
@@ -119,6 +119,7 @@ window.onload = function() {
     let pageReloadObserver = new MutationObserver(function (mutations) {
         mutations.forEach(function (mutation) {
             if (oldHref !== document.location.href) {
+                clearTimeout(timer);
                 oldHref = document.location.href;
                 setTimeout(chessMoveReminder, 1000);
             }
